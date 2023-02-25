1684bea1bfae3bef6ac1da64733d42f1981e219f
Atharva Malji <atharva.malji.2001@gmail.com>
2023-02-25 23:39:59 +0530

    reverted nextjs.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const repo = 'atharvamalji.com'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
-
-
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
 }
 
 module.exports = nextConfig
